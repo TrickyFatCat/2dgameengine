1262f8cadf806f0367af06f8599c5bc0a0eacfbc
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-11-22 22:42:05 +0300

    Implement components map into Entity class

--- src/Entity.h
+++ src/Entity.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 class EntityManager;
 class Component;
@@ -26,14 +27,22 @@ class Entity
 			T* newComponent(new T(std::forward<TArgs>(args)...));
 			newComponent->owner = this;
 			components.emplace_back(newComponent);
+			componentTypeMap[&typeid(*newComponent)] = newComponent;
 			newComponent->Initialize();
 			return *newComponent;
+		}
+
+		template <typename T>
+		T& GetComponent()
+		{
+			return static_cast<T*>(componentTypeMap[&typeid(T)]);
 		}
 
 	private:
 		EntityManager& manager;
 		bool isActive = false;
 		std::vector<Component*> components;
+		std::map<const std::type_info*, Component*> componentTypeMap;
 };
 
 #endif
