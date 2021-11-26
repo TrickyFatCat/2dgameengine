9914cc1e1110bac62032ea02ad32608f3866ba3c
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-11-26 21:57:17 +0300

    Add a chopper entity

--- src/Game.cpp
+++ src/Game.cpp
@@ -123,17 +123,16 @@ void Game::Destroy()
 
 void Game::LoadLevel(const int levelIndex)
 {
-	// TODO add entities and components to them
-
 	// Start including new assets to the assetManager list
-
-	std::string textureFilePath = "./assets/images/tank-big-right.png";
-	assetManager->AddTexture("tank-image", textureFilePath.c_str());
-
+	assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
+	assetManager->AddTexture("chopper-image", std::string("./assets/images/chopper-spritesheet.png").c_str());
+	
 	// Start including entities and also components to them
+	Entity& tankEntity(manager.AddEntity("tank"));
+	tankEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
+	tankEntity.AddComponent<SpriteComponent>("tank-image");
 
-	Entity& newEntity(manager.AddEntity("tank"));
-
-	newEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
-	newEntity.AddComponent<SpriteComponent>("tank-image");
+	Entity& chopperEntity(manager.AddEntity("chopper"));
+	tankEntity.AddComponent<TransformComponent>(64, 64, 20, 20, 32, 32, 1);
+	tankEntity.AddComponent<SpriteComponent>("chopper-image");
 }
