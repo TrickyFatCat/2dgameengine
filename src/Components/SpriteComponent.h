77cf26e27041a0b179076736795d440d14343685
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-11-26 23:09:19 +0300

    Iplement a sprite animation functionality

--- src/Components/SpriteComponent.h
+++ src/Components/SpriteComponent.h
@@ -5,11 +5,51 @@
 #include "../TextureManager.h"
 #include "../AssetManager.h"
 #include "./TransformComponent.h"
+#include "../Animation.h"
 
 class SpriteComponent : public Component
 {
 	public:
-		SpriteComponent(const char* filePath){ SetTexture(filePath); };
+		SpriteComponent(const char* filePath)
+		{
+			isAnimated = false;
+			isFixed = false;
+			SetTexture(filePath); 
+		};
+
+		SpriteComponent(std::string id, int numFrames, int animationSpeed, bool hasDirections, bool isFixed)
+		{
+			isAnimated = true;
+			this->numFrames = numFrames;
+			this->animationSpeed = animationSpeed;
+			this->isFixed = isFixed;
+
+			if (hasDirections)
+			{
+				Animation downAnimation = Animation(0, numFrames, animationSpeed);
+				Animation rightAnimation = Animation(1, numFrames, animationSpeed);
+				Animation leftAnimation = Animation(2, numFrames, animationSpeed);
+				Animation upAnimation = Animation(3, numFrames, animationSpeed);
+
+				animations.emplace("DownAnimation", downAnimation);
+				animations.emplace("RightAnimation", rightAnimation);
+				animations.emplace("LeftAnimation", leftAnimation);
+				animations.emplace("UpAnimation", upAnimation);
+
+				this->animationIndex = 0;
+				this->currentAnimationName = "DownAnimation";
+			}
+			else
+			{
+				Animation singleAnimation = Animation(0, numFrames, animationSpeed);
+				animations.emplace("SingleAnimation", singleAnimation);
+				this->animationIndex = 0;
+				this->currentAnimationName = "SingleAnimation";
+			}
+
+			Play(this->currentAnimationName);
+			SetTexture(id);
+		}
 
 		SDL_RendererFlip spriteFlip = SDL_FLIP_NONE;
 		void SetTexture(std::string assetTextureId) { texture = Game::assetManager->GetTexture(assetTextureId); }
@@ -25,6 +65,13 @@ class SpriteComponent : public Component
 
 		void Update(float deltaTime) override
 		{
+			if (isAnimated)
+			{
+				sourceRectangle.x = sourceRectangle.w * static_cast<int>((SDL_GetTicks() / animationSpeed) % numFrames);
+			}
+
+			sourceRectangle.y = animationIndex * transform->height;
+
 			destinationRectangle.x = static_cast<int>(transform->position.x);
 			destinationRectangle.y = static_cast<int>(transform->position.y);
 			destinationRectangle.w = transform->width * transform->scale;
@@ -36,11 +83,28 @@ class SpriteComponent : public Component
 			TextureManager::Draw(texture, sourceRectangle, destinationRectangle, spriteFlip);
 		}
 
+		void Play(std::string animationName)
+		{
+			numFrames = animations[animationName].numFrames;
+			animationIndex = animations[animationName].index;
+			animationSpeed = animations[animationName].animationSpeed;
+			currentAnimationName = animationName;
+		}
+
 	private:
 		TransformComponent* transform = nullptr;
 		SDL_Texture* texture = nullptr;
 		SDL_Rect sourceRectangle;
 		SDL_Rect destinationRectangle;
+
+		bool isAnimated = false;
+		int numFrames = 0;
+		int animationSpeed = 60;
+		bool isFixed = true;
+		std::map<std::string, Animation> animations;
+		std::string currentAnimationName;
+		unsigned int animationIndex = 0;
+
 };
 
 #endif
