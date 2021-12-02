d1f876f7fefdc41b1c8079132be043e900d21874
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-12-03 00:46:37 +0300

    Update Constants 1. Add entities layers; 2. Make all constants constexpr; 3. Move constants to a namespace;

--- src/Constants.h
+++ src/Constants.h
@@ -1,11 +1,26 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-const unsigned int WINDOW_WIDTH = 800;
-const unsigned int WINDOW_HEIGHT = 600;
+namespace
+{
+	constexpr unsigned int WINDOW_WIDTH = 800;
+	constexpr unsigned int WINDOW_HEIGHT = 600;
 
-const unsigned int FPS = 60;
-const unsigned int FRAME_TARGET_TIME = 1000 / FPS;
-const float DELTA_TIME_MAX = 0.05f;
+	constexpr unsigned int FPS = 60;
+	constexpr unsigned int FRAME_TARGET_TIME = 1000 / FPS;
+	constexpr float DELTA_TIME_MAX = 0.05f;
+
+	enum LayerType
+	{
+		TILEMAP_LAYER = 0,
+		VEGETATION_LAYER = 1,
+		ENEMY_LAYER = 2,
+		PLAYER_LAYER = 3,
+		PROJECTILE_LAYER = 4,
+		UI_LAYER = 5
+	};
+
+	constexpr unsigned int NUM_LAYERS = 6;
+}
 
 #endif
