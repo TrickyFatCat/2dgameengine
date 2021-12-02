281253a983b7d0a20ad0dfe682c3a91b1078034c
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-12-03 01:03:41 +0300

    Increase map scale

--- src/Game.cpp
+++ src/Game.cpp
@@ -132,7 +132,7 @@ void Game::LoadLevel(const int levelIndex)
 	assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
 	assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
 
-	map = new Map("jungle-tiletexture", 1, 32);
+	map = new Map("jungle-tiletexture", 2, 32);
 	map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
 	
 	// Start including entities and also components to them
