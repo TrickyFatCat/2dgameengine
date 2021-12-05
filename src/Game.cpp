b4d7f2b642570dace13dc19804b925080eff244e
TrickyFatCat <tricky.fat.cat@gmail.com>
2021-12-05 03:29:13 +0300

    Add a level finish entity

--- src/Game.cpp
+++ src/Game.cpp
@@ -138,6 +138,7 @@ void Game::LoadLevel(const int levelIndex)
 	assetManager->AddTexture("tank-image", std::string("./assets/images/tank-big-right.png").c_str());
 	assetManager->AddTexture("radar-image", std::string("./assets/images/radar.png").c_str());
 	assetManager->AddTexture("jungle-tiletexture", std::string("./assets/tilemaps/jungle.png").c_str());
+	assetManager->AddTexture("heliport-image", std::string("./assets/images/heliport.png").c_str());
 
 	map = new Map("jungle-tiletexture", 2, 32);
 	map->LoadMap("./assets/tilemaps/jungle.map", 25, 20);
@@ -152,6 +153,11 @@ void Game::LoadLevel(const int levelIndex)
 	tankEntity.AddComponent<TransformComponent>(0, 0, 20, 20, 32, 32, 1);
 	tankEntity.AddComponent<SpriteComponent>("tank-image");
 	tankEntity.AddComponent<ColliderComponent>("ENEMY", 0, 0, 32, 32);
+
+	Entity& heliport(manager.AddEntity("Heliport", OBSTACLE_LAYER));
+	heliport.AddComponent<TransformComponent>(470, 420, 0, 0, 32, 32, 1);
+	heliport.AddComponent<SpriteComponent>("heliport-image");
+	heliport.AddComponent<ColliderComponent>("FINISH", 470, 420, 32, 32);
 
 	Entity& radarEntity(manager.AddEntity("radar", UI_LAYER));
 	radarEntity.AddComponent<TransformComponent>(720, 15, 0, 0, 64, 64, 1);
